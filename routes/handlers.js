9497ebdc51798caed17cc7e49a3ac00835997e8f
Ika Khoza <ikaneng.khoza@gmail.com>
2022-05-06 14:57:27 +0100

    updated handler
    updated handler

--- routes/handlers.js
+++ routes/handlers.js
@@ -4,13 +4,19 @@ const indexHandler = (req, res) => {
   var current_time = new Date();
   request_number += 1;
 
-  res.send(`Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`)
-}
+  res.send(
+    `Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`
+  );
+  console.log(current_time, request_number);
+};
 
 const sayHelloHandler = (req, res) => {
+  var time = new Date();
   const name = req.params.name || "person";
 
   res.send(`Hello ${name}!`);
-}
+  console.log( time, "Lebo");
+};
+
 
 module.exports = { indexHandler, sayHelloHandler };
